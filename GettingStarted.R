6e254a372353094f950009ce22a09eb5ae18c24a
esc358 <200478144@student.georgianc.on.ca>
2025-02-19 09:28:26 -0500

    Data types, Math functions

--- GettingStarted.R
+++ GettingStarted.R
@@ -53,7 +53,84 @@ myvar2 <- "John"
 #TRUE <- "John"
 
 #Data Types
-#TODO: https://www.w3schools.com/r/r_data_types.asp
+#Not need to declare, can be change after
+
+my_var <- 30 # my_var is type of numeric
+my_var <- "Sally" # my_var is now of type character (aka string)
+
+#Basic Data Types
+# numeric - (10.5, 55, 787)
+# integer - (1L, 55L, 100L, where the letter "L" declares this as an integer)
+# complex - (9 + 3i, where "i" is the imaginary part)
+# character (a.k.a. string) - ("k", "R is exciting", "FALSE", "11.5")
+# logical (a.k.a. boolean) - (TRUE or FALSE)
+
+# numeric
+x <- 10.5
+class(x)
+
+# integer
+x <- 1000L
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R is exciting"
+class(x)
+
+# logical/boolean
+x <- TRUE
+class(x)
+
+#TypeConversion
+x <- 1L # integer
+y <- 2 # numeric
+
+# convert from integer to numeric:
+a <- as.numeric(x)
+
+# convert from numeric to integer:
+b <- as.integer(y)
+
+# print values of x and y
+x
+y
+
+# print the class name of a and b
+class(a)
+class(b)
+
+#R Math
+# +
+10 + 5
+
+# -
+10 - 5
+
+#Built-in Math function
+#min()
+min(5, 10, 15)
+
+#max()
+max(5, 10, 15)
+
+#sqrt()
+sqrt(16)
+
+#abs()
+abs(-4.7)
+
+#ceiling()
+ceiling(1.4)
+
+#floor()
+floor(1.4)
+
+#TODO: https://www.w3schools.com/r/r_strings.asp
+
 
 
 
